60b00a5b2670f8b282a6bcbea94002e326f03c04
genlinux <genlinuxworks@gmail.com>
2010-12-26 11:06:17 +0530

    converted to rails 3

---
+++ Dblog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dblog::Application.config.secret_token = '0fc7a4fe435c82fac318b5c72822fe7c0f186b5da7ef82ea9f3c3cf80503ec5d88f5f1eef328cbcd5b4b6ac0e5cb994a4c630d377839876863b27335632c71ed'
